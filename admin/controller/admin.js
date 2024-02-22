2ab6142e2da084f66e71a0d4292ce22d0c4ab4dd
Tuan Tran <caotri8801@gmail.com>
2024-02-22 17:22:34 +0700

    6th commit1

--- admin/controller/admin.js
+++ admin/controller/admin.js
@@ -9,14 +9,19 @@ const getElmntId = (id) => {
 // get product list
 const getProductList = () => {
     let promise = services.getPrd()
-    promise.then((data)=>{
+    promise.then((data) => {
         // console.log("data: ", data);
-        renderProductList(data.data)
+        let filtCon = getElmntId('filter').value
+        let sortCon = getElmntId('sortByPrice').value
+
+        let productList = productFilterSort(filtCon, sortCon, data.data)
+
+        renderProductList(productList)
     })
 
-    .catch((error)=>{
-        console.log("error: ", error);
-    })
+        .catch((error) => {
+            console.log("error: ", error);
+        })
 }
 getProductList()
 
@@ -27,22 +32,22 @@ const getDataFromForm = () => {
     let product = null
 
     let name = getElmntId('name').value
-    flag &= val.nullCheck(name,'tbName','Name must not be null!')
-    
+    flag &= val.nullCheck(name, 'tbName', 'Name must not be null!')
+
     let price = getElmntId('price').value
-    flag &= val.nullCheck(price,'tbPrice','Price must not be null!') && val.patternCheck(price,/^[0-9]+$/,'tbPrice','Price must be number!')
+    flag &= val.nullCheck(price, 'tbPrice', 'Price must not be null!') && val.patternCheck(price, /^[0-9]+$/, 'tbPrice', 'Price must be number!')
 
     let screen = getElmntId('screen').value
-    flag &= val.nullCheck(screen,'tbScreen','Screen must not be null!')
+    flag &= val.nullCheck(screen, 'tbScreen', 'Screen must not be null!')
 
     let backCamera = getElmntId('backCamera').value
-    flag &= val.nullCheck(backCamera,'tbBackCamera','Back CMR must not be null!')
+    flag &= val.nullCheck(backCamera, 'tbBackCamera', 'Back CMR must not be null!')
 
     let frontCamera = getElmntId('frontCamera').value
-    flag &= val.nullCheck(frontCamera,'tbFrontCamera','Font CMR must not be null!')
+    flag &= val.nullCheck(frontCamera, 'tbFrontCamera', 'Font CMR must not be null!')
 
     let img = getElmntId('image').value
-    flag &= val.nullCheck(img,'tbImage','Image must not be null!')
+    flag &= val.nullCheck(img, 'tbImage', 'Image must not be null!')
 
     let desc = getElmntId('description').value
 
@@ -50,19 +55,19 @@ const getDataFromForm = () => {
     const index = getElmntId('type').selectedIndex
     flag &= val.checkSelect(index, 'tbType', 'Please select phone type!')
 
-    if(flag){
-        product = new Product(name,price,screen,backCamera,frontCamera,img,desc,type)
+    if (flag) {
+        product = new Product(name, price, screen, backCamera, frontCamera, img, desc, type)
     }
-     
+
 
     return product
 
 }
 // render Product List
 const renderProductList = (productList) => {
-    let htmlContent =''
-    
-    for( let i = 0; i < productList.length; i++){
+    let htmlContent = ''
+
+    for (let i = 0; i < productList.length; i++) {
         let prd = productList[i]
         htmlContent += `
         <tr>
@@ -103,7 +108,7 @@ const cancelFunct = () => {
     getElmntId('formLogin').reset();
     divs = document.getElementsByClassName('sp-thongbao');
 
-    [].slice.call( divs ).forEach(function ( div ) {
+    [].slice.call(divs).forEach(function (div) {
         div.innerHTML = '';
     });
 
@@ -121,21 +126,21 @@ const showProductOnForm = (product) => {
     getElmntId('type').value = product.type
 }
 // edit product
-const editProduct = (id) =>{
+const editProduct = (id) => {
     getElmntId('modal-footer').innerHTML = `
             <button id="btnUpdate" type="button" class="btn btn-success m-1" onclick='updateProduct(${id})'>Update</button>
             <button id="btnCancel" type="button" class="btn btn-danger m-1" onclick='cancelFunct()'>Cancel</button>
             </td>
     `
     const promise = services.getPrdDetailById(id)
-    .then((data)=>{
-        console.log("data: ", data.data);
-        showProductOnForm(data.data)
-    })
+        .then((data) => {
+            console.log("data: ", data.data);
+            showProductOnForm(data.data)
+        })
 
-    .catch((error)=>{
-        console.log("error: ", error);
-    })
+        .catch((error) => {
+            console.log("error: ", error);
+        })
 
     // const product = 
 }
@@ -143,78 +148,123 @@ const editProduct = (id) =>{
 // Save product into mock api
 const saveProduct = () => {
     let product = getDataFromForm()
-    if(product){
+    if (product) {
         const promise = services.addPrd(product)
-        .then((data)=>{
-            getProductList()
-        })
-    
-        .catch((error)=>{
-            console.log("error: ", error);
-        })
-        
+            .then((data) => {
+                getProductList()
+            })
+
+            .catch((error) => {
+                console.log("error: ", error);
+            })
+
         cancelFunct()
     }
-    
+
 }
 
 // Delete Product
 const deleteProduct = (id) => {
     const flag = confirm('Are you sure to delete!')
-    if(flag){
+    if (flag) {
         const promise = services.deletePrd(id)
-        .then((data)=>{
-            getProductList()
-        })
-    
-        .catch((error)=>{
-            console.log("error: ", error);
-        })
+            .then((data) => {
+                getProductList()
+            })
+
+            .catch((error) => {
+                console.log("error: ", error);
+            })
     }
 }
 
 // Update Product
 const updateProduct = (id) => {
     const product = getDataFromForm()
-    if(product){
+    if (product) {
         const promise = services.editPrd(id, product)
-        .then((data)=>{
-            getProductList()
-        })
-    
-        .catch((error)=>{
-            console.log("error: ", error);
-        })
+            .then((data) => {
+                getProductList()
+            })
+
+            .catch((error) => {
+                console.log("error: ", error);
+            })
         cancelFunct()
     }
 }
 
 // Search product
-getElmntId('search').onkeyup = function(){
+getElmntId('search').onkeyup = function () {
     var valueSearch = getElmntId('search').value
     var valueSearchLowerCase = valueSearch.toLowerCase()
 
     let productList
     let productListSearch = []
     let promise = services.getPrd()
-    
-    promise.then((data)=>{
-        productList =  data.data
+
+    promise.then((data) => {
+        let filtCon = getElmntId('filter').value
+        let sortCon = getElmntId('sortByPrice').value
+
+        let productList = productFilterSort(filtCon, sortCon, data.data)
 
         // Duyệt mảng 
-        for(let i = 0; i < productList.length; i++){
+        for (let i = 0; i < productList.length; i++) {
             let product = productList[i]
             var productName = product.name.toLowerCase()
-            if(productName.indexOf(valueSearchLowerCase) !== -1){
+            if (productName.indexOf(valueSearchLowerCase) !== -1) {
                 productListSearch.push(product)
             }
         }
         renderProductList(productListSearch)
-        
+
     })
 
-    .catch((error)=>{
-        console.log("error: ", error);
-    })
-   
+        .catch((error) => {
+            console.log("error: ", error);
+        })
+
+}
+
+// Process product list by filter and sort
+
+const productFilterSort = (filtCon, sortCon, productList) => {
+    let arrFilter
+    if (filtCon != 'all') {
+        arrFilter = productList.filter((value) => value.type == filtCon)
+    } else {
+        arrFilter = productList
+    }
+    if (sortCon == 'low') {
+
+        // A compare function that compares the age property of two objects
+        function compareByAge(a, b) {
+            return a.price - b.price;
+        }
+
+        // Sort the array by age in ascending order
+        arrFilter.sort(compareByAge);
+    }
+    if (sortCon == 'high') {
+        // A compare function that compares the age property of two objects
+        function compareByAge(a, b) {
+            return b.price - a.price;
+        }
+
+        // Sort the array by age in ascending order
+        arrFilter.sort(compareByAge);
+    }
+    return arrFilter
+}
+
+// Action when selecting sort and filter
+getElmntId('filter').onchange = () => {
+    getElmntId('search').value = ''
+    getProductList()
+}
+
+getElmntId('sortByPrice').onchange = () => {
+    getElmntId('search').value = ''
+    getProductList()
 }
